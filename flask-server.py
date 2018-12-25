89be04260d396ec73c68e23976775278f50b1bfa
Ravinder Singh <ravi_29_12@hotmail.com>
2018-12-25 20:26:28 +0000

    First Commit.

---
+++ flask-server.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+from restaurant_crud import RestaurantCRUD
+from flask import Flask, render_template, request, url_for, redirect, jsonify, flash
+
+app = Flask(__name__)
+crud = RestaurantCRUD()
+#
+@app.route("/")
+@app.route("/restaurant")
+def restaurant():
+    restaurants = crud.get_all_restaurants()
+    return render_template("restaurant.html", restaurants=restaurants)
+
+@app.route("/restaurant/new", methods=["GET", "POST"])
+def newRestaurant():
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "CREATE":
+            crud.create_restaurant(data["name"])
+            flash("New Restaurant Created!")
+        return redirect(url_for('restaurant'))
+    else:
+        return render_template("restaurant_op.html", restaurant="", op="add")
+
+@app.route("/restaurant/<int:r_id>/edit", methods=["GET", "POST"])
+def editRestaurant(r_id):
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "UPDATE":
+            crud.update_restaurant_name(r_id, data["name"])
+        return redirect(url_for('restaurant'))
+    else:
+        r = crud.get_restaurant(r_id)
+        return render_template("restaurant_op.html", restaurant=r, op="edit")
+
+@app.route("/restaurant/<int:r_id>/delete", methods=["GET", "POST"])
+def deleteRestaurant(r_id):
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "DELETE":
+            crud.delete_restaurant(r_id)
+        return redirect(url_for('restaurant'))
+    else:
+        r = crud.get_restaurant(r_id)
+        return render_template("restaurant_op.html", restaurant=r, op="delete")
+
+@app.route("/restaurant/<int:r_id>/", methods =["GET", "POST"])
+def restaurantMenuItem(r_id):
+    r = crud.get_restaurant(r_id)
+    m = crud.get_rest_menu_items(r_id)
+    return render_template("menu.html", restaurant=r, items=m)
+
+@app.route("/restaurant/<int:r_id>/<int:m_id>/edit", methods=["GET", "POST"])
+def editMenuItem(r_id, m_id):
+    r = crud.get_restaurant(r_id)
+    m = crud.get_menu_item(r.id, m_id)
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "UPDATE":
+            crud.update_menu_item(r.id, m.id, data["name"], data["course"], data["description"], data["price"])
+        return redirect(url_for('restaurantMenuItem', r_id=r.id))
+    else:
+        return render_template("menuitem_op.html", restaurant=r, menu_item=m, op="edit")
+
+@app.route("/restaurant/<int:r_id>/<int:m_id>/delete", methods=["GET", "POST"])
+def deleteMenuItem(r_id, m_id):
+    r = crud.get_restaurant(r_id)
+    m = crud.get_menu_item(r.id, m_id)
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "DELETE":
+            crud.delete_menu_item(r.id, m.id)
+        return redirect(url_for('restaurantMenuItem', r_id=r.id))
+    else:
+        return render_template("menuitem_op.html", restaurant=r, menu_item=m, op="delete")
+
+@app.route("/restaurant/<int:r_id>/new", methods=["GET", "POST"])
+def newMenuItem(r_id):
+    r = crud.get_restaurant(r_id)
+    m = crud.get_rest_menu_items(r_id)
+    if request.method == "POST":
+        data = request.form
+        if data["action"] == "CREATE":
+            crud.create_menu_item(r.id, data["name"], data["description"], data["course"], data["price"])
+            flash("New menu item created!")
+        return redirect(url_for('restaurantMenuItem', r_id=r.id))
+    else:
+        return render_template("menuitem_op.html", restaurant=r, menu_item='', op="add")
+
+@app.route("/restaurant/<int:r_id>/menu/JSON", methods=["GET"])
+def getRestaurantMenuItemJSON(r_id):
+    menu_items = crud.get_rest_menu_items(r_id)
+    print(menu_items[0])
+    return jsonify(MenuItems=[{
+        "name":i.name,
+        "price":i.price,
+        "course":i.course,
+        "description":i.description
+        } for i in menu_items])
+
+if __name__ == "__main__":
+    app.secret_key = "super secret key"
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
---
+++ restaurant_crud.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# CRUD operation on database
+# C = Create
+# R = Read
+# U = Update
+# D = Delete
+
+# configuration
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from database_setup import Restaurant, MenuItem, Base
+
+class RestaurantCRUD:
+    # initialize
+    def __init__(self):
+        engine = create_engine("sqlite:///restaurantmenu.db?check_same_thread=False")
+        Base.metadata.bind = engine
+        DBsession = sessionmaker(bind=engine)
+        self.session = DBsession()
+
+    # Create
+    def create_restaurant(self, name):
+        restaurant = Restaurant(name=name)
+        self.session.add(restaurant)
+        self.session.commit()
+
+    # Read
+    def get_all_restaurants(self):
+        restaurants = self.session.query(Restaurant).all()
+        return restaurants
+
+    def get_restaurant(self, id):
+        restaurant = self.session.query(Restaurant).filter_by(id=id).one()
+        return restaurant
+
+    # Update
+    def update_restaurant_name(self, id, new_name):
+        restaurant = self.session.query(Restaurant).filter_by(id=id).one()
+        restaurant.name = new_name
+        self.session.add(restaurant)
+        self.session.commit()
+
+    # Delete
+    def delete_restaurant(self, id):
+        restaurant = self.session.query(Restaurant).filter_by(id=id).one()
+        self.session.delete(restaurant)
+        self.session.commit()
+
+    def get_menu_items(self):
+        menu_items = self.session.query(MenuItem).all()
+        return menu_items
+
+    def get_rest_menu_items(self, r_id):
+        r_menu_items = self.session.query(MenuItem).filter_by(restaurant_id=r_id).all()
+        return r_menu_items
+    
+    def get_menu_item(self, r_id, m_id):
+        menu_item = self.session.query(MenuItem).filter_by(restaurant_id=r_id, id=m_id).one()
+        return menu_item
+    
+    def update_menu_item(self, r_id, m_id, new_name='', new_course='', new_desc='', new_price=''):
+        menu_item = self.session.query(MenuItem).filter_by(restaurant_id=r_id, id=m_id).one()
+        if new_name:
+            menu_item.name = new_name
+        if new_desc:
+            menu_item.description = new_desc
+        if new_price:
+            menu_item.price = new_price
+        if new_course:
+            menu_item.course = new_course
+        return menu_item
+    
+    def delete_menu_item(self, r_id, m_id):
+        menu_item = self.session.query(MenuItem).filter_by(restaurant_id=r_id, id=m_id).one()
+        self.session.delete(menu_item)
+        self.session.commit()
+
+    def create_menu_item(self, r_id, name, description, course, price):
+        menu_item = MenuItem()
+        menu_item.name = name
+        menu_item.description = description
+        menu_item.course = course
+        menu_item.price = price
+        menu_item.restaurant = self.session.query(Restaurant).filter_by(id=r_id).one()
+        self.session.add(menu_item)
+        self.session.commit()
