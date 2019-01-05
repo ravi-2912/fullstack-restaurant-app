8cb7ce4bbe7dc6d1226dc576449dbdba471848fb
Ravinder Singh <ravi_29_12@hotmail.com>
2019-01-05 22:20:01 +0000

    Updated to add user table, establish user foreign keys and update data file.

--- database_setup.py
+++ database_setup.py
@@ -8,34 +8,63 @@ from sqlalchemy import create_engine
 # configuration
 Base = declarative_base()
 
+# class for sql table for users
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 # class for sql table
 class Restaurant(Base):
-    # table name
-    __tablename__ = "restaurant"
-    # mapper
-    name = Column(String(80), nullable=False)
+    __tablename__ = 'restaurant'
+
     id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 # class for sql table
 class MenuItem(Base):
-    # table name
-    __tablename__ = "menu_item"
-    # mapper
+    __tablename__ = 'menu_item'
+
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
-    course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
-
-    # relationships
-    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
 
 def main():
     # configuration end of file
-    engine = create_engine('sqlite:///restaurantmenu.db')
+    #engine = create_engine('sqlite:///restaurantmenuwithusers.db')
     # for PostgreSQL
-    # engine = create_engine("postgresql://user:password@localhost:portNumber/restaurantmenu", echo=True)
+    engine = create_engine("postgresql://ravi_:ravi_@localhost:5432/restaurantmenuwithusers", echo=True)
     Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
